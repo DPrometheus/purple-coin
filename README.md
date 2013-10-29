85df464b2da046d6eb42faadd603a52054f1e8cf
DPrometheus <michael_siroen@outlook.com>
2013-10-29 11:14:38 +0100

    Time and Port changes

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-FooCoin (FOO)
+PurpleCoin (PURPLE)
 
 Blah, blah, blah.
--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("FOO");
-    case mBTC: return QString("mFOO");
-    case uBTC: return QString::fromUtf8("μFOO");
+    case BTC: return QString("PURPLE");
+    case mBTC: return QString("mPURPLE");
+    case uBTC: return QString::fromUtf8("μPURPLE");
     default: return QString("???");
     }
 }
@@ -45,13 +45,13 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("FooCoin");
-    case mBTC: return QString("milliFooCoin (1 / 1,000)");
-    case uBTC: return QString("microFooCoin (1 / 1,000,000)");
+    case BTC: return QString("PurpleCoin");
+    case mBTC: return QString("milliPurpleCoin (1 / 1,000)");
+    case uBTC: return QString("microPurpleCoin (1 / 1,000,000)");
     default: return QString("???");
     }
 }
-//a single unit (.00000001) of FooCoin is called a "wander."
+//a single unit (.00000001) of PurpleCoin is called a "wander."
 qint64 BitcoinUnits::factor(int unit)
 {
     switch(unit)
